cc5551afb4a43ef18cbcc4dbd16e6bff7787f691
niko bazylev <42851684+bazylevnik0@users.noreply.github.com>
2021-03-21 15:25:29 +0300

    trying add textures

--- version-0.1/script.js
+++ version-0.1/script.js
@@ -1,45 +1,54 @@
+//init variables
 var camera, scene, light, light_ambient, renderer;
-var geometry, material, mesh;
 
+//set loaders
+const loader = new OBJLoader();
+const loader_texture = new THREE.TextureLoader();
+	
 init();
 
 function init() {
-	//set loaders
-	const loader = new OBJLoader();
-	const loader_texture = new OBJLoader();
-		
-	//scene
-	scene = new THREE.Scene();
-	//table of scene objects:
-	//[0]: light point;
-	//[1]: light ambient;
-	//[2]: obj sky;
-	//[3]: obj back;	
-	
 	//camera
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 	camera.position.set(0, 0, 5);
 	
-	//light
-	//point
+	//scene
+	scene = new THREE.Scene();
+	//table of scene children objects:
+	//[0]: light point;
+	//[1]: light ambient;
+	//[2]: obj sky;
+	//[3]: obj back;	
+	//[4]: obj earth;
+
+		
+	//point light
 	light = new THREE.PointLight( 0xffffff, 1, 8 );
 	light.position.set( 0, 0, 0 );
+	//add point light in scene
 	scene.children[0] = light;
-	//ambient
+	
+	//ambient light
 	light_ambient = new THREE.AmbientLight( 0x404040 );
+	//add ambient light in scene
 	scene.children[1] = light_ambient;
 	
 		
 	/*test_obj
-	geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
-	material = new THREE.MeshPhongMaterial({color: 0xffffff}); 
-    	mesh = new THREE.Mesh( geometry, material );
+	let geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
+	let material = new THREE.MeshPhongMaterial({color: 0xffffff}); 
+    	let mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 	*/
-
+	
 	//load .obj objects
 	//sky
 	loader.load('obj/sky.obj', (obj) => {
+		let texture = loader_texture.load('img/back_texture.jpg')
+	
+		obj.children[0].material.map = texture;
+		
+		//add obj in scene
 		scene.children[2] = obj;
 		//set start position & scale
 		scene.children[2].position.set(0,0,0);
@@ -47,19 +56,51 @@ function init() {
 	});
 	//back
 	loader.load('obj/back.obj', (obj) => {
-		obj.children[0].material.map = loader_texture.load('img/back_texture.jpg');
-		
 		scene.children[3] = obj;
+	
+	
+
+
+		/*temp version1
+		let texture = loader_texture.load('img/back_texture.jpg')
+		obj.children[0].material.map = texture;
+		scene.children[3] = obj;
+		*/
+
+		/*temp version3
+		let texture = loader_texture.load('img/back_texture.jpg')
+			obj.children[0].material = new THREE.MeshStandardMaterial();
+		let temp_mesh = new THREE.Mesh( obj.children[0].geometry,  obj.children[0].material );
+		//temp_mesh.material.map = texture;
+		temp_mesh.traverse(function (child) {   // aka setTexture
+                if (child instanceof THREE.Mesh) {
+                    child.material.map = texture;
+                }
+           	 });
+		scene.children[3] = temp_mesh;
+		*/
+
+		/*temp version2
+		obj.children[0].material = new THREE.MeshStandardMaterial();
+		obj.children[0].material.map = texture; 
+
+		obj.children[0].geometry.computeVertexNormals();
+		let temp_mesh = new THREE.Mesh( obj.children[0].geometry,  obj.children[0].material );
+		scene.children[3] = temp_mesh;	
+		*/
+
+		//add obj in scene
 		//set start position & scale
 		scene.children[3].scale.set( 7, 7, 7);
 		scene.children[3].position.set(0,-2.5,0);
 	});
-		//earth
+	//earth
 	loader.load('obj/earth/earth.obj', (obj) => {
+		//add obj in scene
 		scene.children[4] = obj;
 		//set start position & scale
 		scene.children[4].scale.set( 5, 5, 5);
-		scene.children[4].position.set(0,-4,0);
+		scene.children[4].position.set(0,-4.5,0);
 	});
 	
 	

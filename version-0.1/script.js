1688d615bf19cca54b6e0045133a414e9611cfe6
niko bazylev <42851684+bazylevnik0@users.noreply.github.com>
2021-05-03 20:27:02 +0300

    add function show & move

--- version-0.1/script.js
+++ version-0.1/script.js
@@ -5,14 +5,24 @@ var camera, scene, light, light_ambient, renderer;
 const loader = new OBJLoader();
 const loader_texture = new THREE.TextureLoader();
 const loader_material = new MTLLoader();
+
+//obj for control
+const pointer = new THREE.Vector2();
+var raycaster = new THREE.Raycaster();
+
+//logic
+var score = 0;
+var game = false;
+var move_array = [];	
+
 	
 init();
 
 function init() {
+//init
 	//camera
-	//camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
-	camera.position.set(0, -4, 6.75)	
+	camera.position.set(0, -4, 6)	
 	camera.rotation.set(-0.3,0,0)
 	//scene
 	scene = new THREE.Scene();
@@ -48,13 +58,6 @@ function init() {
 	);
 	*/
 
-	//table of scene children objects:
-	//[0]: light point;
-	//[1]: light ambient;
-	//[2]: obj sky;
-	//[3]: obj back;	
-	//[4]: obj earth;
-
 		
 	//point light
 	light = new THREE.PointLight( 0xffffff, 1, 25 );
@@ -66,11 +69,8 @@ function init() {
 	light_ambient = new THREE.AmbientLight( 0x404040 );
 	//add ambient light in scene
 	scene.children[1] = light_ambient;
-										//!!!напиши функцию для пересчета цветов и ввода через человеческий вид
-										//и функцию смены цвета материала
-	
-
-	
+		
+	//all
 	loader_material.load('obj/all.mtl', function( material ) {
 						     	material.preload();
 						     	loader.setMaterials( material );
@@ -83,88 +83,137 @@ function init() {
 											scene.children[2] = object;
 											scene.children[2].position.set( 0, 0, 0 );
 										  	//set start position & scale
-											scene.children[2].scale.set( 2, 2, 2 );
+											scene.children[2].scale.set( 5, 5, 5 );
 											scene.children[2].position.set( 0, -7, 0 );
 										    	scene.children[2].rotation.y = 4;
-										    }
+										  	//set status changeable
+											scene.children[2].children[1].material.changeable = 1;
+											scene.children[2].children[2].material.changeable = 1;
+											scene.children[2].children[4].material.changeable = 1;
+											scene.children[2].children[5].material.changeable = 1;
+										}
 						     	);
 					     }
 	);
-	
+
+
+//activate
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	let loading = setInterval( function() { 
+					//activate animation loop
 				 	if (scene.children[scene.children.length-1] !== undefined) { 
 						clearInterval(loading); 
 						renderer.setAnimationLoop( animation ); 
-					} 
-		      } , 1000 );
-
+				 	} 
+					
+					//set blank color property r
+					for (let i = 0; i < scene.children[2].children.length; i++ ) {
+						scene.children[2].children[i].material.color.r = 0;
+					}
+				 } , 1000 );
 }
 
+//control
+	document.addEventListener( 'mousedown', mouse_down );
+	function mouse_down( event ) {
+		pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+		var intersection = raycaster.intersectObject( scene.children[2], true );
+		if ( intersection.length > 0 ) {
+			if (intersection[ 0 ].object.material.changeable === 1) {
+			intersection[ 0 ].object.material.color.r = 1;
+			intersection[ 0 ].object.material.color.r = 1;
+			}
+		}	
+	}	
+
+
+//logic
+	var game_object;
+	function show() {
+		//hide game objects
+		scene.children[2].children[4].visible = false;
+		scene.children[2].children[5].visible = false;
+		//set game objects to start position 
+		scene.children[2].children[4].position.set( 0.25 , 0.05 , -0.25 );
+		scene.children[2].children[5].position.set( 0.25 , 0.05 , -0.25 );
+		//collect game objects to array
+		let objects = [ scene.children[2].children[4] , scene.children[2].children[5] ];
+		//choose random game object
+		game_object = objects[ Math.round(Math.random()) ];
+		game_object.visible = true;
+		//add game object  to animation move array
+		move_array.push( [game_object, new THREE.Vector3( 0.125 , -0.25 , -0.125 ) , 0.01] );
+		return 1;
+	}
+	
+
+//render
 var inhale = true, exhale = false, size = 1;
 function animation( time ) {
-	/*
-	 //life animation
+	 //life
 	 if (inhale === true) {
 			size -= 0.01;
 			if (size > -0.5) {
-				
-				//!!!добавь комментарии не красиво
-				scene.children[5].position.y -= 0.001;
-				scene.children[6].position.y += 0.001;
-				scene.children[7].position.y += 0.001;
-				scene.children[9].rotation.x -= 0.0005;
-				scene.children[10].rotation.x -= 0.0005;  
-				scene.children[10].rotation.z -= 0.0005;
-				//green
-					//right
-						
-						//1
-							//green-1
-							scene.children[12].rotation.x -= 0.00025;
-							//green-2
-							scene.children[13].rotation.x -= 0.00025;
-						//2
-							//green-1
-							scene.children[15].rotation.x -= 0.00025;
-							//green-2
-							scene.children[16].rotation.x -= 0.00025;
-							//green-3
-							scene.children[17].rotation.x -= 0.00025;
-				
+	
+			
 			} else { inhale = false; exhale =  true; }
 		
-		 }
+	 }
 	 if (exhale === true) {
 			size += 0.01; 
 			if (size < 0.5) {
-				scene.children[5].position.y += 0.001;
-				scene.children[6].position.y -= 0.001;
-				scene.children[7].position.y -= 0.001;
-				scene.children[9].rotation.x += 0.0005;
-				scene.children[10].rotation.x += 0.0005;  
-				scene.children[10].rotation.z += 0.0005;
-				//green
-					//right
-						//1
-							//green-1
-							scene.children[12].rotation.x += 0.00025;
-							//green-2
-							scene.children[13].rotation.x += 0.00025;
-						//2
-							//green-1
-							scene.children[15].rotation.x += 0.00025;
-							//green-2
-							scene.children[16].rotation.x += 0.00025;
-							//green-3
-							scene.children[17].rotation.x += 0.00025;
+			
 			} else { inhale = true; exhale =  false; }
 		
+	 }
+	
+	//move
+	move();
+	//control
+	raycaster.setFromCamera( pointer, camera );
+	
+	//render	
+	renderer.render(scene, camera); 
+}
+
+function move() {
+	//move_array.push( [game_object, new THREE.Vector3( 0, 0, 0 ) , 0.01] );
+	if ( move_array.length > 0 ) {
+		for ( let i = 0; i < move_array.length; i++ ) {
+			//check: it is object? 
+			if ( move_array[i][0] !== undefined ) {
+				let temp = move_array[i][0];
+				//transform to obj
+				temp.move = {};
+				temp.move.destination = move_array[i][1];
+				temp.move.step = move_array[i][2];
+				temp.move.way = temp.position.distanceTo(temp.move.destination); 
+				move_array[i] = temp;
+			} else {
+				let temp = move_array[i];
+				//check: obj in the end way?
+				if ( temp.move.way >= temp.move.step * 2 ) {
+					//move obj
+					//x
+					if ( temp.position.x > temp.move.destination.x ) temp.position.x -= temp.move.step;
+					if ( temp.position.x < temp.move.destination.x ) temp.position.x += temp.move.step;
+					//y
+					if ( temp.position.y > temp.move.destination.y ) temp.position.y -= temp.move.step;
+					if ( temp.position.y < temp.move.destination.y ) temp.position.y += temp.move.step;
+					//z
+					if ( temp.position.z > temp.move.destination.z ) temp.position.z -= temp.move.step;
+					if ( temp.position.z < temp.move.destination.z ) temp.position.z += temp.move.step;
+					//recount way
+					temp.move.way = temp.position.distanceTo(temp.move.destination); 
+					move_array[i] = temp;
+				} else {
+					//delete obj
+					move_array.splice( i , i + 1 );
+				}
+			}
+		}	
 	}
-	 scene.children[3].rotation.y += 0.0005;
-				*/						    
-	 //render	
-	 renderer.render(scene, camera); 
 }

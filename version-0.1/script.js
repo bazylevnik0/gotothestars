e435a8784deab725b74e7d4609057f035400453e
niko bazylev <42851684+bazylevnik0@users.noreply.github.com>
2021-05-07 20:46:50 +0300

    adding textures to portals try posteffects

--- version-0.1/script.js
+++ version-0.1/script.js
@@ -1,5 +1,5 @@
 //init variables
-var camera, scene, light, light_ambient, renderer;
+var camera, scene, light, light_ambient, renderer , composer;
 
 //set loaders
 const loader = new OBJLoader();
@@ -22,8 +22,10 @@ function init() {
 //init
 	//camera
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
-	camera.position.set(0, -4, 6)	
-	camera.rotation.set(-0.3,0,0)
+	camera.position.set( 0 , -4 , 6)	
+	camera.rotation.set( -0.3 , 0, 0 )
+	camera.layers.enable( 0 );
+	camera.layers.enable( 1 );
 	//scene
 	scene = new THREE.Scene();
 	
@@ -75,26 +77,43 @@ function init() {
 						     	material.preload();
 						     	loader.setMaterials( material );
 						     	loader.load('obj/all.obj', (object) => {
-						     					//set color
-											object.children[0].material.color.r = 1;
-											object.children[0].material.color.g = 1;
-											object.children[0].material.color.b = 1;
-											//add obj in scene
-											scene.children[2] = object;
-											scene.children[2].position.set( 0, 0, 0 );
-										  	//set start position & scale
-											scene.children[2].scale.set( 5, 5, 5 );
-											scene.children[2].position.set( 0, -7, 0 );
-										    	scene.children[2].rotation.y = 4;
-										  	//set statuses
-											//changeable
-											scene.children[2].children[1].material.changeable = 1;
-											scene.children[2].children[2].material.changeable = 1;
-											//form
-											scene.children[2].children[1].form = "cube";
-											scene.children[2].children[4].form = "cube";
-											scene.children[2].children[2].form = "sphere";
-											scene.children[2].children[5].form = "sphere";
+						     					//add obj in scene
+												scene.children[2] = object;
+											//set clear under color
+												//portal right
+													scene.children[2].children[0].material.color.r = 1;
+													scene.children[2].children[0].material.color.g = 1;
+													scene.children[2].children[0].material.color.b = 1;
+												//portal left
+													scene.children[2].children[1].material.color.r = 1;
+													scene.children[2].children[1].material.color.g = 1;
+													scene.children[2].children[1].material.color.b = 1;
+												//back
+													scene.children[2].children[4].material.color.r = 1;
+													scene.children[2].children[4].material.color.g = 1;
+													scene.children[2].children[4].material.color.b = 1;
+											//set normal maps
+												//portal right
+													scene.children[2].children[0].material.normalMap = loader_texture.load( 'obj/normal.png' );
+												//portal right
+													scene.children[2].children[1].material.normalMap = loader_texture.load( 'obj/normal.png' );
+											//set start position & scale
+												scene.children[2].scale.set( 5, 5, 5 );
+												scene.children[2].position.set( 0, -7, 0 );
+										    		scene.children[2].rotation.y = 4;
+										  	//set statuses 									!!!ПЕРЕДЕЛАТЬ
+												//set info about changeable
+													scene.children[2].children[5].material.changeable = 1;
+													scene.children[2].children[6].material.changeable = 1;
+												//set info about form
+													scene.children[2].children[5].form = "cube";
+													//.layers.set(1);
+													//scene.children[2].children[5].layers.enable(1);
+													scene.children[2].children[5].layers.enable(1);
+													scene.children[2].children[2].form = "cube";
+													scene.children[2].children[6].form = "sphere";
+													scene.children[2].children[6].layers.enable(1);
+													scene.children[2].children[3].form = "sphere";
 										}
 						     	);
 					     }
@@ -111,10 +130,6 @@ function init() {
 						clearInterval(loading); 
 						renderer.setAnimationLoop( animation ); 
 				 	} 
-					
-					//set blank color property r
-					scene.children[2].children[1].material.color.set( 0,0,0 )
-					scene.children[2].children[2].material.color.set( 0,0,0 )
 				 } , 1000 );
 }
 
@@ -136,7 +151,26 @@ function init() {
 	}	
 
 
+//posteffect
+	composer = new EffectComposer(renderer);
+	composer.addPass(new RenderPass(scene, camera));
 
+	const bloomPass = new BloomPass(
+     		 1,    // strength
+     		 250,   // kernel size
+      		 400,    // sigma ?
+      		 2,  // blur render target resolution
+  	);
+  	composer.addPass(bloomPass);
+
+ 	 const filmPass = new FilmPass(
+      		10,   // noise intensity
+      		0.025,  // scanline intensity
+      		648,    // scanline count
+     		false,  // grayscale
+ 	 );
+  	filmPass.renderToScreen = true;
+  	composer.addPass(filmPass);
 	
 
 //render
@@ -165,20 +199,47 @@ function animation( time ) {
 	processing();
 
 	//render	
-	renderer.render(scene, camera); 
+	//renderer.render(scene, camera); 
+	//composer.render();
+//requestAnimationFrame(animation);
+
+	renderer.autoClear = false;
+
+	 renderer.clear();
+  
+  camera.layers.set(1);
+  composer.render();
+  
+  renderer.clearDepth();
+  camera.layers.set(0);
+  renderer.render(scene, camera);
+
+/*
+requestAnimationFrame(animation(time));
+renderer.autoClear = false;
+
+  renderer.clear();
+  
+  camera.layers.set(1);
+  composer.render();
+  
+  renderer.clearDepth();
+  camera.layers.set(0);
+  renderer.render(scene, camera);
+*/
 }
 
 //logic
 	var game_object;
 	function show () {
 		//hide game objects
-		scene.children[2].children[4].visible = false;
-		scene.children[2].children[5].visible = false;
+		scene.children[2].children[2].visible = false;
+		scene.children[2].children[3].visible = false;
 		//set game objects to start position 
-		scene.children[2].children[4].position.set( 0.25 , 0.05 , -0.25 );
-		scene.children[2].children[5].position.set( 0.25 , 0.05 , -0.25 );
+		scene.children[2].children[2].position.set( 0.25 , 0.05 , -0.25 );
+		scene.children[2].children[3].position.set( 0.25 , 0.05 , -0.25 );
 		//collect game objects to array
-		let objects = [ scene.children[2].children[4] , scene.children[2].children[5] ];
+		let objects = [ scene.children[2].children[2] , scene.children[2].children[3] ];
 		//choose random game object
 		game_object = objects[ Math.round(Math.random()) ];
 		game_object.visible = true;
@@ -190,10 +251,10 @@ function animation( time ) {
 		if ( game_object.form === select.form ) {
 			++score;
 			game_object.position.set( 0.4 , -0.4 , -0.4 );
-			if ( select.form === "cube") {
+			if ( select.form === "sphere") {
 				move_array.push( [game_object, new THREE.Vector3( 0.65 , -0.7 , -0.2 ) , 0.01] );
 			}
-			if ( select.form === "sphere") {
+			if ( select.form === "cube") {
 				move_array.push( [game_object, new THREE.Vector3( -0.1 , -0.8 , -0.8 ) , 0.01] );
 			}
 		} else {
